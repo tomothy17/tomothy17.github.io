14554da1810cf1cbf3dfb93222891e474e6f6f71
tomothy17 <116015850+tomothy17@users.noreply.github.com>
2022-10-29 15:01:56 +0100

    Update clickscript.js

--- clickscript.js
+++ clickscript.js
@@ -1,12 +1,3 @@
-int clicks_num = 0;
-	function func() {
-    	var text = document.getElementById("demo");
-    	if (text.innerHTML == "foo"){
-        	text.innerHTML = "bar";
-        }
-        else {
-        	text.innerHTML = "foo";
-        }
-    clicks_num += 1
-    document.getElementById("clicks").innerHTML = "clicks: "+clicks_num
-    }
+function func() {
+	document.getElementById("demo").innerHTML = "uas"
+}
